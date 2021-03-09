74e347250397903f85164e5e9150127b2243d567
liaolonghui <1340881930@qq.com>
2021-03-09 21:11:03 +0800

    promise的then

--- 手写bind，节流防抖，promise，deepClone，去重等以及各种算法/promise/promise.js
+++ 手写bind，节流防抖，promise，deepClone，去重等以及各种算法/promise/promise.js
@@ -42,12 +42,14 @@ function Promise(executor){
 // then
 // 注意then的返回结果是一个Promise对象
 Promise.prototype.then = function(onResolved, onRejected){
+  const self = this
   return new Promise((resolve, reject) => {
 
-    if (this.promiseStatus === 'resolved') {
+    // 封装一下
+    function callback(type){
       try {
         // 执行成功的回调并且获取到成功回调的返回值
-        const result = onResolved(this.PromiseValue)
+        const result = type(self.PromiseValue)
         // 看看result是不是Promise
         if (result instanceof Promise) {
           // 是Promise
@@ -64,24 +66,12 @@ Promise.prototype.then = function(onResolved, onRejected){
       }
     }
 
+    if (this.promiseStatus === 'resolved') {
+      callback(onResolved)
+    }
+
     if (this.promiseStatus === 'rejected') {
-      try {
-        // 执行成功的回调并且获取到成功回调的返回值
-        const result = onRejected(this.PromiseValue)
-        // 看看result是不是Promise
-        if (result instanceof Promise) {
-          // 是Promise
-          result.then( v => {
-            resolve(v)
-          }, r => {
-            reject(r)
-          })
-        } else {
-          resolve(result)
-        }
-      } catch (error) {
-        reject(error)
-      }
+      callback(onRejected)
     }
 
     // 但如果是pending状态则需要
@@ -89,8 +79,12 @@ Promise.prototype.then = function(onResolved, onRejected){
       // 因为还是penging状态，所以此时可以把回调函数先保存下来
       // 当状态改变时再调用，即resolve或者reject执行时调用
       this.callbacks.push({
-        onResolved,
-        onRejected
+        onResolved: function(){
+          callback(onResolved)
+        },
+        onRejected: function(){
+          callback(onRejected)
+        }
       })
     }
 
